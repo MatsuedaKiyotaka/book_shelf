7e7fe5405cd68934615190e59f6d4a4d7efe03c6
MatsuedaKiyotaka <osotodeasobow365@icloud.com>
2019-02-14 18:57:15 +0900

    レビュー機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).per(4)
+  end
+  
+  def show
   end
 
   def new
@@ -16,16 +21,11 @@ class BooksController < ApplicationController
     end
   end
 
-  def show
-    @book = Book.find(params[:id])
-  end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to @book, notice: "書籍を更新しました。"
     else
@@ -34,7 +34,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path, notice: "書籍を削除しました。"
   end
@@ -42,6 +41,11 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :price, :publish_date, :description, :new_image)
+    params.require(:book).permit(:title, :price, :publish_date, :description, :new_image, :category_id)
   end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end 
+
 end
